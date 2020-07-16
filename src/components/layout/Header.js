89312caa0ab731602f19c84c32bb094a1946b536
Deirdre18 <deirdreweldon18@gmail.com>
2020-07-16 21:07:46 +0100

    updating index html and Header.js

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -2,33 +2,30 @@
 
 // functional component is one return, which works like render does
 
-import React from 'react'
+
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Header() {
-    return (
-        //will be some links for about page but won't do that just yet
-        <header style={headerStyle}>
-            <h1>TodoList</h1>
-            <Link style={ linkStyle } to="/">Home</Link> | <Link style={ linkStyle } to="/">About</Link>
-        </header>
-        )
+  return (
+    <header style={headerStyle}>
+      <h1>TodoList</h1>
+      <Link style={linkStyle} to="/">Home</Link> | <Link style={linkStyle} to="/about">About</Link>
+    </header>
+  )
 }
-
 //exporting function
 
-const headerStyle={
-
-    background: '#333',
-    color: '#fff',
-    textAlign: 'center',
-    padding: '10px'
-    
+const headerStyle = {
+  background: '#333',
+  color: '#fff',
+  textAlign: 'center',
+  padding: '10px'
 }
 
 const linkStyle = {
-    color: '#fff',
-    textDecoration: 'none'
+  color: '#fff',
+  textDecoration: 'none'
 }
 
 export default Header;
